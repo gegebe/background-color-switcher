0b175987245ef95bf85616106d1e72ebb5c53e21
gegebe <ggirbes@gmail.com>
2023-10-19 21:44:38 +0200

    con for y sin

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 // Recuperamos TODOS los elementos HTML que cumplen con la condición class="button"
 const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
+
+////////////////////////////////////////
+//// Con target y parámetros
+////////////////////////////////////////
+
+// document.addEventListener("click", function(event){
+//   //let backGrey = document.querySelector("#grey").id;
+//   //.target devuelve el objeto en el que se ha hecho clic
+//   let colour;
+//   colour = event.target.id;
+//   console.log(colour);
+//   body.style.backgroundColor = colour; 
+// }) 
+
+////////////////////////////////////////
+//// Recorriendo los buttons con forEach
+////////////////////////////////////////
+
+buttons.forEach (function(button){
+  button.addEventListener("click", function(evento){
+    let color;
+    color = evento.target.id;
+    console.log(color);
+    body.style.backgroundColor = color;
+  })
+})
